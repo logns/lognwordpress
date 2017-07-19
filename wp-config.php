2812a94aed6ab8efec3e92a55df900106a7aa861
EC2 Default User <ec2-user@ip-172-31-0-95.ap-south-1.compute.internal>
2017-07-19 10:43:29 +0000

    Init Commit

---
+++ wp-config.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'log_db');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'logn123$');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'p;dTvI}z5R*FO-hD1d!qU!_=zWh*Xv7K,5I5-G(*b)p+xsk?vRe|b}+Tvc49E~&A');
+define('SECURE_AUTH_KEY',  'W2=eYyl#5tMl5X;Q7A>1yZlh0}O!xw-jJOUN$(}57`SjF|yPMlV*2WhB3GJxq:L5');
+define('LOGGED_IN_KEY',    'L8G1F/A$Oj$m_LG:#Pbkb1uZIiN6At?M.YJh*aHpD{J?lko0!:?bzMT1|||]Ho:z');
+define('NONCE_KEY',        'do=F/.zoT6K}6NGk);,{bR}?+P0T.d}-4)n$<HzWbQEN<|,X<un1-(u?v7Y</koX');
+define('AUTH_SALT',        'sf~-t|ldjeXf#O$XElc<p/(%.zI4mDWDt17z*B]ue0|w_2?CtEly>j]Q=vuJvN{i');
+define('SECURE_AUTH_SALT', '|UXnmdpsPu*cW> w,6/pyx|;Hk P*IP ~X6$g)0%PkhZO3v[/.;?y!h79s7OgYWQ');
+define('LOGGED_IN_SALT',   '>L7JLUh/?j#TB)_MH%Z`CM{4xEjq6(6tN`[2R-oKl;;JqUMB%d_z*nYT1-ghUnle');
+define('NONCE_SALT',       'Jk6~G%yW5-7La{Z*Lk><]mYv{|u`@v4[y f%va#6=[4lC32ra0%|WafQ[lts^KeS');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+//set_time_limit => 60;
---
+++ wp-content/plugins/wp-custom-cssjs/readme.txt
@@ -0,0 +1,55 @@
+=== Custom CSS/JS ===
+Contributors: johnibom
+
+Tags: css, js, jquery, javascript, tracking pixel, custom, css js
+Requires at least: 3.5
+Tested up to: 4.7
+Stable tag: 1.2
+
+WP Custom CSSJS plugin allows you to insert CSS, Javascript, jQuery and Tracking Pixel easily on your wordpress site right from your dashboard.
+
+== Description ==
+
+Welcome to WP Custom CSS JS by PieSolutions.
+So you want to put CSS, Javascript, Jquery or Tracking Pixel on your Website without messing up with your theme's files? This small and light weight plugin gives you ability to do this right from the wordpress dashboard.
+
+
+=Features=
+
+
+**Custom CSS in Header**
+
+**Custom JS/jQuery in Header**
+
+**Custom CSS in Footer**
+
+**Custom JS/jQuery in Footer**
+
+== Installation ==
+Just follow the simple steps mention on the plugin page:
+http://www.pie-solutions.com/wp-custom-cssjs/
+
+
+== LOCALIZATION ==
+* We are looking for translators. Please reach out to us and/or translate this plugin to your own language here: https://translate.wordpress.org/projects/wp-plugins/wp-custom-cssjs
+
+== CHANGELOG ==
+=v1.2 December 28 2016=
+** Fixes **
+*Fixed text domain for translation.
+
+=v1.1 August 11 2015=
+** Fixes **
+*Hook Footer CSS and JS on WP_FOOTER Hook.
+
+=v1.00 June 16 2015=
+**New Features:**
+
+*Custom CSS Box for Header.
+*Custom JS/jQuery Box for Header
+*Custom CSS Box for Footer
+*Custom JS/jQuery Box for Footer
+
+== Screenshots ==
+
+1. Backend
